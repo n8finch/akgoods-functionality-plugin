19e706dd78c76f34d1efd47a897d4102006e6582
Nate Finch <finchproservices@gmail.com>
2016-03-08 17:48:39 +0900

    added button and left area

--- inc/category-page.php
+++ inc/category-page.php
@@ -25,14 +25,14 @@ add_action( 'woocommerce_before_main_content', 'add_category_title_and_descripti
 function add_category_title_and_description_to_page() {
 	if ( is_product_category() ) {
 		global $wp_query;
-		$cat             = $wp_query->get_queried_object();
-		$cat_name        = $cat->name; //gets termID
-		$termID = $cat->term_id; //gets termID
+		$cat         = $wp_query->get_queried_object();
+		$cat_name    = $cat->name; //gets termID
+		$termID      = $cat->term_id; //gets termID
 		$topSEOfield = get_field( 'category_page_top_seo_description', 'product_cat_' . $termID );
 
-		echo    '<div class="category-header">
+		echo '<div class="category-header">
 				<h2 class="category-header-name">' . $cat_name . '</h2>' .
-		        '<p class="category-header-description">'. $topSEOfield . '</p></div>';
+		     '<p class="category-header-description">' . $topSEOfield . '</p></div>';
 	}
 }
 
@@ -40,14 +40,16 @@ add_action( 'woocommerce_archive_description', 'woocommerce_category_image', 2 )
 function woocommerce_category_image() {
 	if ( is_product_category() ) {
 		global $wp_query;
-		$cat          = $wp_query->get_queried_object(); //gets category meta
-		$thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); //gets thumbnail ID
+		$cat             = $wp_query->get_queried_object(); //gets category meta
+		$thumbnail_id    = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); //gets thumbnail ID
+		$cat_name        = $cat->name; //gets termID
 		$cat_description = $cat->description; //gets termID
-		$image        = wp_get_attachment_url( $thumbnail_id ); //gets image thumbnail
+		$image           = wp_get_attachment_url( $thumbnail_id ); //gets image thumbnail
 		if ( $image ) {
-			echo '<div class="category-hero">';
-			echo '<img src="' . $image . '" alt="" />';
-			echo $cat_description;
+			echo '<div class="category-hero" style="background-image: url(' . $image . ');">';
+			echo '<div class="category-description-left">' . $cat_description . ' ';
+			echo '<div class="category-description-button">View All<br/>' . $cat_name . '</div>';
+			echo '</div>';
 			echo '</div>';
 
 		}
